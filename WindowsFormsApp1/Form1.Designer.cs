19c20833962887234bea35a960669fdaf87348f6
Tiwari <381508@cognizant.com>
2019-07-09 16:41:01 +0530

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnProcessFIle = new System.Windows.Forms.Button();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnProcessFIle
+            // 
+            this.btnProcessFIle.Location = new System.Drawing.Point(382, 105);
+            this.btnProcessFIle.Name = "btnProcessFIle";
+            this.btnProcessFIle.Size = new System.Drawing.Size(75, 23);
+            this.btnProcessFIle.TabIndex = 0;
+            this.btnProcessFIle.Text = "Procees File";
+            this.btnProcessFIle.UseVisualStyleBackColor = true;
+            this.btnProcessFIle.Click += new System.EventHandler(this.btnProcessFIle_Click);
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Location = new System.Drawing.Point(382, 177);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(0, 13);
+            this.lblCount.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.btnProcessFIle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnProcessFIle;
+        private System.Windows.Forms.Label lblCount;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private  async void btnProcessFIle_Click(object sender, EventArgs e)
+        {
+
+            //lblCount.Text = "Processing file. Please wait...";
+            //int count = CountCharacters();
+            //lblCount.Text = count.ToString() + " characters in file";
+            // Create a task to execute CountCharacters() function
+            // CountCharacters() function returns int, so we created Task<int>
+            Task<int> task = new Task<int>(CountCharacters);
+            task.Start();
+
+            lblCount.Text = "Processing file. Please wait...";
+            // Wait until the long running task completes
+            int count = await task;
+            lblCount.Text = count.ToString() + " characters in file";
+        }
+
+        private int CountCharacters()
+        {
+            int count = 0;
+
+            //count = count + 30;
+           // Create a StreamReader and point it to the file to read
+            using (StreamReader reader = new StreamReader("D:\\Working\\Working\\Data.txt"))
+            {
+                string content = reader.ReadToEnd();
+                count = content.Length;
+                // Make the program look busy for 5 seconds
+                Thread.Sleep(15000);
+            }
+            //Thread.Sleep(5000);
+            return count;
+        }
+
+    }
+}
